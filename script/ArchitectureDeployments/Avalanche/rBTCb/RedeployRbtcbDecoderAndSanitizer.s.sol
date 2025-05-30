536d169d3c44966e49328a0f082d861766817a84
midnight-commit <43814957+midnight-commit@users.noreply.github.com>
2025-05-30 11:03:20 +0200

    fix contract name

--- script/ArchitectureDeployments/Avalanche/rBTCb/RedeployRbtcbDecoderAndSanitizer.s.sol
+++ script/ArchitectureDeployments/Avalanche/rBTCb/RedeployRbtcbDecoderAndSanitizer.s.sol
@@ -11,7 +11,7 @@ import "forge-std/Script.sol";
  *  source .env && forge script script/ArchitectureDeployments/Avalanche/rBTCb/RedeployRbtcbDecoderAndSanitizer.s.sol:RedeployRbtcbDecoderAndSanitizer --broadcast --verifier-url 'https://api.routescan.io/v2/network/mainnet/evm/43114/etherscan' --etherscan-api-key "verifyContract" --verify
  * @dev Optionally can change `--with-gas-price` to something more reasonable
  */
-contract RedeployRsAvaxDecoderAndSanitizer is Script, AvalancheAddresses {
+contract RedeployRbtcbDecoderAndSanitizer is Script, AvalancheAddresses {
     uint256 public privateKey;
 
     address public boringVault = 0xe684F692bdf5B3B0DB7E8e31a276DE8A2E9F0025;
